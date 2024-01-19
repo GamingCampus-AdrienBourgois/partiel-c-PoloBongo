8226d7db0971cbacca0131c03a6e9e23b3f4f699
PoloBongo <earthrp.polo@gmail.com>
2024-01-19 14:47:36 +0100

    exo3

--- StudentLibrary/Solution3.cpp
+++ StudentLibrary/Solution3.cpp
@@ -15,16 +15,7 @@ void Solution3::SetWords(const std::vector<std::string>& _words)
 
 void Solution3::SortWords() 
 {
-    if (words.empty())
-    {
-        throw std::runtime_error("Le vecteur est vide !");
-    }
-
-    std::sort(words.begin(), words.end(), [](const std::string& a, const std::string& b) {
-        return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end(),
-        [](char c1, char c2) { return std::tolower(c1) < std::tolower(c2); });
-        });
-
+	(words.empty()) ? throw std::runtime_error("Le vecteur est vide!") : std::sort(words.begin(), words.end());
 }
 
 std::vector<std::string> Solution3::GetSortedWords() const
